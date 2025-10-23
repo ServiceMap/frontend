eee8bb2e0e314f87f0b483ef7836b753b68464e8
Andrew Yehozha <andrew.yehozha@tesseris.com>
2025-10-23 13:46:41 +0200

    Fail tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import App from './App'
 describe('App', () => {
     it('renders ServiceMap Admin Portal heading', () => {
         render(<App />)
-        expect(screen.getByText('ServiceMap Admin Portal')).toBeInTheDocument()
+        expect(screen.getByText('ServiceMap Admin Portal1')).toBeInTheDocument()
     })
 })
