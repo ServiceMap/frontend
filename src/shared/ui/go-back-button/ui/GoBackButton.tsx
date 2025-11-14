66ef5a9d5b088e227ed101917bfb32051320c47f
Andrew Yehozha <andrew.yehozha@tesseris.com>
2025-11-14 17:26:41 +0100

    Fix

--- src/shared/ui/go-back-button/ui/GoBackButton.tsx
+++ src/shared/ui/go-back-button/ui/GoBackButton.tsx
@@ -1,20 +1,22 @@
-import { historyBackOrDefault } from "@/shared/lib";
+import { cn, historyBackOrDefault } from "@/shared/lib";
 import { Button } from "@/shared/ui";
 
 interface GoBackButtonProps {
   fallbackUrl: string;
   stepsBack?: number;
   text: string;
+  className?: string;
 }
 
 export const GoBackButton: React.FC<GoBackButtonProps> = ({
   fallbackUrl,
   stepsBack = 1,
   text,
+  className,
 }) => {
   return (
     <Button
-      className="tw:cursor-pointer"
+      className={cn("tw:cursor-pointer", className)}
       onClick={() => historyBackOrDefault(fallbackUrl, stepsBack)}
     >
       {text}
