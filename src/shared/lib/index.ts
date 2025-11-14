1717d5cd468f8490f0a64f9c319f043a17037f4d
Andrew Yehozha <andrew.yehozha@tesseris.com>
2025-11-14 19:33:31 +0100

    Add local storage

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,5 +1,6 @@
 export { cn } from "@/shared/lib/cn.ts";
 export * from "@/shared/lib/css";
+export * from "@/shared/lib/local-storage";
 export * from "@/shared/lib/navigation";
 export * from "@/shared/lib/types";
 export * from "@/shared/lib/ui";
--- src/shared/ui/theme-provider/lib/actions.ts
+++ src/shared/ui/theme-provider/lib/actions.ts
@@ -1,10 +1,12 @@
+import { getLocalStorageItem, setLocalStorageItem } from "@/shared/lib";
 import { THEME_LOCAL_STORAGE_KEY } from "@/shared/ui/theme-provider/config";
 import { isThemeMode, type ThemeMode } from "@/shared/ui/theme-provider/lib";
 
 export const getStoredTheme = (): ThemeMode | null => {
-  const stored = localStorage.getItem(THEME_LOCAL_STORAGE_KEY);
+  const stored = getLocalStorageItem<ThemeMode>(THEME_LOCAL_STORAGE_KEY);
   return isThemeMode(stored) ? stored : null;
 };
 
-export const setStoredTheme = (theme: ThemeMode) =>
-  localStorage.setItem(THEME_LOCAL_STORAGE_KEY, theme);
+export const setStoredTheme = (theme: ThemeMode) => {
+  setLocalStorageItem(THEME_LOCAL_STORAGE_KEY, theme);
+};
